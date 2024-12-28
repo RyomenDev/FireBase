f50dbe8345636655e63c837fe5ff63a9a0ec4197
RyomenDev <aakashvatsh007@gmail.com>
2024-12-28 09:06:14 +0530

    js-firebase

--- client(javscript)/src/api/index.jsx
+++ client(javscript)/src/api/index.jsx
@@ -1,29 +1,3 @@
 import { LoginUser } from "./authApi.jsx";
-import {
-  fetchUserProfile,
-  updateUserProfile,
-  UserSubscription,
-} from "./userApi.jsx";
-import { createPaymentIntent } from "./stripeApi.jsx";
-import { registerCall } from "./retellAi.jsx";
-import {
-  createPatient,
-  getPatientsByDoctor,
-  //   getPatientById,
-  updatePatient,
-  deletePatient,
-} from "./doctorsApi";
 
-export {
-  LoginUser,
-  fetchUserProfile,
-  updateUserProfile,
-  createPaymentIntent,
-  registerCall,
-  createPatient,
-  getPatientsByDoctor,
-  //   getPatientById,
-  updatePatient,
-  deletePatient,
-  UserSubscription,
-};
+export { LoginUser };
